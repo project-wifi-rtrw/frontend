227f47caeda2b64441d82af1fbbd828bd0dce857
RiskevO3 <91577090+RiskevO3@users.noreply.github.com>
2023-10-16 18:55:12 +0700

    adding cookies handling

--- src/app/pages/auth/[tokenId]/page.js
+++ src/app/pages/auth/[tokenId]/page.js
@@ -1,13 +1,29 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 
-const ID = ({params}) => {
+function Page({params}) {
   const {tokenId} = params;
+  const [kukis, setKukisName] = useState("");
+  const [cookies, setCookie, removeCookie] = useCookies(['name']);
+
+  const setCookieHandler = () =>{
+    console.log('workingg')
+    setCookie('name',kukis,{path:'/'})
+    console.log(cookies.name)
+  }
+
   return (
     <>
     <p>Your Token : {tokenId}</p>
+    <input type="text" value={kukis} onChange={(e)=>setKukisName(e.target.value)}/>
+    <p className={'text-white'}>kukis value : {kukis}</p>
+    <br></br><br></br><br></br>
+    <button id="buttonLogin" onClick={setCookieHandler}>
+      Set Cookie
+    </button>
     </>
   )
 };
 
-export default ID;
+export default Page;
